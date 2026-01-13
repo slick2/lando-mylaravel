a8b4f68019821fd6440753fe976ef31faabf310f
slick2 <carey.dayrit@gmail.com>
2026-01-13 21:51:08 +0800

    controller chapter

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return "welcome, laravel";
-});
+Route::get('/', [welcomeController::class, 'index']);
 
 Route::get('/home', function () {
    return view('home', ['name' => "xCarey"]);
